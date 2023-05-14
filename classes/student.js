a355c58e32ff66198d620b49ca329c8ee62927b8
Viktor Villner <hal-macbook@Viktors-MacBook-Pro.local>
2023-05-14 14:29:31 +0200

    all tests passed

--- classes/student.js
+++ classes/student.js
@@ -2,6 +2,27 @@ const Person = require('./person');
 
 // Your code here
 
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA){
+    super(firstName, lastName);
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  static compareGPA(student1, student2){
+    const gpa1 = student1.GPA;
+    const gpa2 = student2.GPA;
+
+    if (gpa1 === gpa2){
+      return 'Both students have the same GPA';
+    } else if (gpa1 > gpa2){
+      return `${student1.firstName} ${student1.lastName} has the higher GPA.`;
+    } else {
+      return `${student2.firstName} ${student2.lastName} has the higher GPA.`;
+    }
+  }
+}
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
--- classes/teacher.js
+++ classes/teacher.js
@@ -2,6 +2,19 @@ const Person = require('./person');
 
 // Your code here
 
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience){
+    super(firstName,lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers){
+    const sumExperience = teachers.reduce((accumulator, currentEle,) => accumulator + currentEle.yearsOfExperience, 0);
+    return sumExperience;
+  }
+}
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
